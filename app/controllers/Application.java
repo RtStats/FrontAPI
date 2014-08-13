718c6d196b0b95925f3e25ed957b26a8599b2042
Thanh Nguyen <btnguyen2k@gmail.com>
2014-08-14 00:56:02 +0700

    Minor update

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,13 +7,16 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-	public static Promise<Result> noGet(String param) {
-		Promise<Result> promise = Promise.promise(new Function0<Result>() {
-			public Result apply() throws Exception {
-				response().setContentType("application/json");
-				return ok("{\"status\":405,\"message\":\"GET is not supported!\"}");
-			}
-		});
-		return promise;
-	}
+    /*
+     * Handles GET:/*
+     */
+    public static Promise<Result> noGet(String param) {
+        Promise<Result> promise = Promise.promise(new Function0<Result>() {
+            public Result apply() throws Exception {
+                response().setContentType("application/json");
+                return ok("{\"status\":405,\"message\":\"GET is not supported!\"}");
+            }
+        });
+        return promise;
+    }
 }
